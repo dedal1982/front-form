60b482c62e9c1f178583d081b24ff8fcfc67f8dc
aldm <d44.a1@yandex.ru>
2024-12-29 21:32:19 +0300

    1

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-// Получаем все секции
 const sections = document.querySelectorAll("section");
 let currentSectionIndex = 0;
 
-// Функция для прокрутки к следующей секции
 function scrollToSection(index) {
   sections[index].scrollIntoView({ behavior: "smooth" });
 }
 
-// Обработчик события прокрутки
 window.addEventListener("wheel", (event) => {
   if (event.deltaY > 0) {
-    // Прокрутка вниз
     if (currentSectionIndex < sections.length - 1) {
       currentSectionIndex++;
       scrollToSection(currentSectionIndex);
     }
   } else {
-    // Прокрутка вверх
     if (currentSectionIndex > 0) {
       currentSectionIndex--;
       scrollToSection(currentSectionIndex);
     }
   }
 });
+
+//переключение пшеницы
+const softWrapper = document.querySelector(".soft__wrapper");
+const solidWrapper = document.querySelector(".solid__wrapper");
+const softBottom = document.querySelector(".soft__bottom-left");
+const solidBottom = document.querySelector(".solid__bottom-left");
+
+if (softBottom) {
+  softBottom.addEventListener("click", () => {
+    softWrapper.classList.remove("active");
+    solidWrapper.classList.add("active");
+  });
+}
+
+if (solidBottom) {
+  solidBottom.addEventListener("click", () => {
+    solidWrapper.classList.remove("active");
+    softWrapper.classList.add("active");
+  });
+}
