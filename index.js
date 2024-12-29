9f272efac1e4d79ddc37301850181f3aaebf4a5f
aldm <d44.a1@yandex.ru>
2024-12-29 21:08:32 +0300

    1

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-// // По событию скрола
-// window.addEventListener("scroll", function () {
-//   // Найти блок к которому нужно проскролить
-//   const element = document.querySelector("#target");
+// Получаем все секции
+const sections = document.querySelectorAll("section");
+let currentSectionIndex = 0;
 
-//   // скролим к элементу
-//   element.scrollIntoView({ behavior: "smooth" });
-// });
+// Функция для прокрутки к следующей секции
+function scrollToSection(index) {
+  sections[index].scrollIntoView({ behavior: "smooth" });
+}
+
+// Обработчик события прокрутки
+window.addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    // Прокрутка вниз
+    if (currentSectionIndex < sections.length - 1) {
+      currentSectionIndex++;
+      scrollToSection(currentSectionIndex);
+    }
+  } else {
+    // Прокрутка вверх
+    if (currentSectionIndex > 0) {
+      currentSectionIndex--;
+      scrollToSection(currentSectionIndex);
+    }
+  }
+});
